763e29cc46b91ccc305920a878c7d081a8a34789
cindyangXQ <yangc2105@gmail.com>
2024-03-21 01:12:42 +0800

    Avoid creating file of same name

--- README.md
+++ README.md
@@ -7,6 +7,9 @@ Created in concern of privacy issues with uploading files to online sites.
 
 ## Convert `.txt` to `.csv` file
 
-Download file `txt_to_csv.py` and run. 
+Download file [`txt_to_csv.py`](https://github.com/cindyangXQ/my-tools/blob/main/txt_to_csv.py) and run.
+`py txt_to_csv.py`
 
-Read txt file, split on `space` `' '`, and write to a csv file. 
+Read txt file, split on `space`, and write to a csv file. 
+
+New file located in the same directory as the txt file.  
--- txt_to_csv.py
+++ txt_to_csv.py
@@ -1,16 +1,21 @@
 import csv
+import os
+from create_file import get_filepath_to_create
 
-def read():
-    filepath = input('File path of txt to convert to csv: ')
-    filepath = filepath or 'text.txt'
-    with open('new_csv.csv', 'w', encoding='utf-8') as csvfile:
+def txt_to_csv():
+    txt_path = input('File path of txt to convert to csv: ')
+    if txt_path[-4:] != '.txt'or not os.path.exists(txt_path):
+        raise FileNotFoundError('Please enter a valid txt file path: ' + txt_path)
+    csv_path = get_filepath_to_create(txt_path[:-4] + '.csv')
+
+    with open(csv_path, 'x', encoding='utf-8') as csvfile:
         writer = csv.writer(csvfile)
-        with open(filepath, encoding='utf-8') as txtfile:
+        with open(txt_path, 'r', encoding='utf-8') as txtfile:
             for row in txtfile:
                 words = list(filter(lambda word: len(word) > 0, row.rstrip('\n').split(' ')))
                 if len(words) == 0: continue
                 writer.writerow(words)
 
-read()
+txt_to_csv()
                 
             
